d982a71b676ac4011e531c92a1cc8d07b93e9cb3
Ankit Kumar Bhati <ankitbhati883@gmail.com>
2022-08-21 13:20:02 +0530

    theme

---
+++ script.js
@@ -0,0 +1,27 @@
+function yellowTheme() {
+    document.getElementById("container").style.backgroundColor =
+      "yellow";
+    document.getElementById("textarea").style.backgroundColor = "yellow";
+    document.getElementById("textarea").style.color = "white";
+    document.getElementById("textarea").style.borderColor = "white";
+  }
+  function pinkTheme() {
+    document.getElementById("container").style.backgroundColor = "pink";
+    document.getElementById("textarea").style.backgroundColor = "pink";
+    document.getElementById("textarea").style.color = "white";
+    document.getElementById("textarea").style.borderColor = "white";
+  }
+  
+  function redTheme() {
+    document.getElementById("container").style.backgroundColor = "red";
+    document.getElementById("textarea").style.backgroundColor = "red";
+    document.getElementById("textarea").style.color = "black";
+    document.getElementById("textarea").style.borderColor = "black";
+  }
+  
+  function greenTheme() {
+    document.getElementById("container").style.backgroundColor = "green";
+    document.getElementById("textarea").style.backgroundColor = "green";
+    document.getElementById("textarea").style.color = "black";
+    document.getElementById("textarea").style.borderColor = "black";
+  }
